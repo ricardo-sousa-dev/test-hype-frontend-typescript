df8869c57c0d1beebbb75ca4c48fe6c0626607f9
ricardo-sousa <rwmsousa@gmail.com>
2022-06-01 19:50:04 -0300

    feat: create context project

---
+++ src/context/Provider.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Context from './Context';
+
+function Provider({ children }) {
+  const [ resultSearchBar, setResultSearchBar ] = useState('');
+  const [ viewProductDetails, setViewProductDetails ] = useState('');
+  const [ totalCart, setTotalCart ] = useState(0);
+
+  const providerValue = {
+    resultSearchBar, setResultSearchBar,
+    viewProductDetails, setViewProductDetails,
+    replaceSpecialChars, totalCart, setTotalCart,
+  };
+
+  return (
+    <Context.Provider value={providerValue}>
+      {children}
+    </Context.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
